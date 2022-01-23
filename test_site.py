e74f4f7878d24e259c34767e27b1aa6f3d3b7783
00gogo00 <zachgalaxy@comcast.net>
2022-01-22 18:09:00 -0800

    added basic post request functionality

--- test_site.py
+++ test_site.py
@@ -1,33 +1,46 @@
 from flask import Flask, request
 from scraper import scrape
-from time import time()
+import pickle
+
 app = Flask(__name__)
 
 
 
 
 
+n = 0
 
-
-@app.route("/")
-lastRun=0
-contents=""
+@app.route("/", methods = ['GET', 'POST'])
 def test_site():
-    if lastRun-time>600:
-        results = scrape()
-        output = []
-        output.append('<ul>')
-        for l1 in results:
-            output.append(f'<li>{l1[0]}</li>')
-            output.append('<ul>')
-            for l2 in l1[1]:
-                output.append(f'<li>{l2[0]}</li>')
-                output.append('<ul>')
-                for l3 in l2[1]:
-                    output.append(f'<li>{l3}</li>')
-                output.append('</ul>')
-            output.append('</ul>')
-        output.append('</ul>')
-        contents=''.join(output)
-     return contents
-    
+    if request.method == 'GET':
+        return test_site_get(request)
+    if request.method == 'POST':
+        test_site_post(request)
+        return test_site_get(request)
+
+def test_site_post(request):
+    global n
+    n += 1
+
+def test_site_get(request):
+    results = scrape()
+    output = []
+    output.append(f'''
+        <form method="POST">
+        <input type="submit" value="Enter">
+        </form>
+        <p> n is: {n} </p>
+        ''')
+    for l1 in results:
+        output.append(f'<h1>{l1[0]}</h1>')
+        for l2 in l1[1]:
+            output.append(f'<h2>{l2[0]}</h2>')
+            for l3 in l2[1]:
+                output.append(f'''
+                    {l3}
+                    <button>up</button>
+                    <button>down</button>
+                    </br>
+                    ''')
+                              
+    return '\n'.join(output)
