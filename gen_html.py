1e0f82300dcd337d9950c9c870a5fc9b528931d7
rqberry <qberry.ryan@gmail.com>
2022-01-23 22:37:13 -0800

    actual formatting this time

--- gen_html.py
+++ gen_html.py
@@ -9,15 +9,10 @@ def gen_html(database, menu, user):
         <title>Reed Daily Specials {date.today()}</title>
         <style>
         #headerContainer {{
-            height: 105px;
+            height: 70px;
             position: relative;
             background: #870400;
             color: #fff;
-        }}
-        @media only screen and (min-width: 1280px) {{
-            #headerContainer {{
-                height: 130px;
-            }}
         }}
         #header {{
             position: relative;
@@ -25,40 +20,50 @@ def gen_html(database, menu, user):
             max-width: 1280px;
             margin: 0 auto;
         }}
-        #header.reedHeaderCollapse #reedNav {{
-            background: none;
-            position: absolute;
-            top: 15px;
-            left: 100%;
-            border: none;
-            margin: 0;
-            margin-left: -42px;
-            height: 0;
-            width: 27px;
-            overflow: hidden;
-            padding: 20px 0 0 0;
+        h1 {{
+            font-family: verdana;
+            font-size: 46px;
+            font-weight: normal;
+            line-height: normal;
+            margin: 0.5em 0 0.25em;
         }}
-        a {{
-            color: #b12;
-            text-decoration-thickness: 1px!important;
-            text-underline-offset: .2em!important;
+        h2 {{
+            font-family: verdana;
+            font-size: 36px;
+            font-weight: normal;
+            line-height: normal;
+            margin: 0.5em 0 0.25em;
+        }}
+        h3 {{
+            font-family: "calluna", "Times New Roman", Times, serif;
+            font-size: 26px;
+            font-weight: normal;
+            line-height: normal;
+            margin: 0.5em 0 0.5em;
+
+        }}
+        #center_body {{
+            margin-left: 150px;
         }}
         </style>
         </head>
+        ''')
+    output.append('''<body style="background-color:#fffadb;margin:0px;">
         <div id="headerContainer">
-        <header aria-label="Reed header" id="header" class="reedHeaderCollapse">
-        <div id="nameplate">
-        <h1><a title="Rate Reed's Daily Specials">Reed College</a></h1>
-        </div>
+        <header aria-label="Reed header" id="header">
+        <h1 style="font-family:serif;font-size:56px;margin-top:0px;margin-left:20px;"><a>Rate Reed's Daily Specials</a></h1>
         </header>
         </div>
-        ''')
-    output.append('<body style="background-color:#fffadb;">')
+        <div id="center_body">
+    ''')
+
     for category, subcategories in menu:
-        output.append(f'<h1 style="font-family:verdana;">{category}</h1>')
+        output.append(f'<h1>{category}</h1>')
         for subcategory, items in subcategories:
             output.append(f'<h2>{subcategory}</h2>')
             for item in items:
+                output.append('<h3>')
                 output.append(gen_item(item[0], item[1], database.get_item(item[0])))
-    output.append('</body>')
+                output.append('</h3>')
+    output.append('</div> </body>')
     return '\n'.join(output)
