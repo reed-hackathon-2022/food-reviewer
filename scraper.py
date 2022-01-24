9175abea43441c6ca5e2f609d5b643312173500d
rqberry <qberry.ryan@gmail.com>
2022-01-23 18:55:07 -0800

    added timely scraper wrapper

--- scraper.py
+++ scraper.py
@@ -1,5 +1,16 @@
 import urllib.request
+import time
 from bs4 import BeautifulSoup
+
+LAST_RUN = 0
+LAST_RETURN = None
+
+def timely_scrape():
+    if time.time() - LAST_RUN > 600:
+        LAST_RUN = time.time()
+        LAST_RETURN = scrape()
+        return LAST_RETURN
+    return LAST_RETURN
 
 def scrape():
     with urllib.request.urlopen('https://reed.cafebonappetit.com/') as response:
